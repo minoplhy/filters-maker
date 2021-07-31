d43f04f122c2ef64f62bf94d3b4080e2e637cc1d
Minoplhy <zebr@m21.kylz.nl>
2021-07-31 23:27:50 +0700

    Create filters.sh

---
+++ filters.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git clone https://github.com/minoplhy/filters-maker ./filters-maker
+git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_REPO.git ./$Destination_NAME
+filters-maker/crawler.py filters-maker/input/domains.txt
+filters-maker/maker-rpz.py filtes-maker/input/domains.txt $Destination_REPO/$Destination_VERSION/rpz.txt
+cd $Destination_REPO
+git remote add destination https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_REPO.git
+git config --local user.name $GIT_NAME
+git config --local user.email $GIT_EMAIL
+git add .
+git commit -m "Schedule Building : `date`"
+git push -u destination $REPO_BRANCH
