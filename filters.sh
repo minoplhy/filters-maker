c23c8c393d34ce92064b0151a06a36920dd0ccdd
minoplhy <zebr@m21.kylz.nl>
2021-08-01 10:33:27 +0700

    Fixing Attempt #7?

--- filters.sh
+++ filters.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 git clone https://github.com/minoplhy/filters-maker /filters-maker
-git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_REPO.git /$Destination_NAME
+git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_REPO.git /repros
 pip3 install -r /filters-maker/requirements.txt
 python3 /filters-maker/crawler.py /filters-maker/input/domains.txt
-python3 /filters-maker/maker-rpz.py /filtes-maker/input/domains.txt /$Destination_NAME/$Destination_VERSION/rpz.txt
-cd /$Destination_NAME
+python3 /filters-maker/maker-rpz.py /filtes-maker/input/domains.txt /repros/$Destination_VERSION/rpz.txt
+cd /repros
 git remote add destination https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_REPO.git
 git config --local user.name $GIT_NAME
 git config --local user.email $GIT_EMAIL
