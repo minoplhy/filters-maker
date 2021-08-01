f2400dc230e91c372cba48c84a8fab5903a9db48
minoplhy <zebr@m21.kylz.nl>
2021-08-01 10:48:16 +0700

    Fixing Attempt #9?

--- filters.sh
+++ filters.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 git clone https://github.com/minoplhy/filters-maker /filters-maker
-git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_REPO.git /repros
+git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_UNAME/$Destination_REPO.git /repros
 pip3 install -r /filters-maker/requirements.txt
 python3 /filters-maker/crawler.py /filters-maker/input/domains.txt
 python3 /filters-maker/maker-rpz.py /filtes-maker/input/domains.txt /repros/$Destination_VERSION/rpz.txt
 cd /repros
-git remote add destination https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_UNAME/$Destination_REPO.git
 git config --local user.name $GIT_NAME
 git config --local user.email $GIT_EMAIL
 git add .
 git commit -m "Schedule Building : `date`"
-git push -u destination $REPO_BRANCH
+git push -u origin $REPO_BRANCH
