50bcdb8aa6bc61e092a754f165d715dc7b2d1ad9
minoplhy <zebr@m21.kylz.nl>
2021-08-01 10:55:08 +0700

    Fixing Attempt #9?

--- filters.sh
+++ filters.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 git clone https://github.com/minoplhy/filters-maker /filters-maker
-git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_UNAME/$Destination_REPO.git /repros
+git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_Destination_UNAME/$INPUT_Destination_REPO.git /repros
 pip3 install -r /filters-maker/requirements.txt
 python3 /filters-maker/crawler.py /filters-maker/input/domains.txt
-python3 /filters-maker/maker-rpz.py /filtes-maker/input/domains.txt /repros/$Destination_VERSION/rpz.txt
+python3 /filters-maker/maker-rpz.py /filtes-maker/input/domains.txt /repros/$INPUT_Destination_VERSION/rpz.txt
 cd /repros
-git config --local user.name $GIT_NAME
-git config --local user.email $GIT_EMAIL
+git config --local user.name $INPUT_GIT_NAME
+git config --local user.email $INPUT_GIT_EMAIL
 git add .
 git commit -m "Schedule Building : `date`"
-git push -u origin $REPO_BRANCH
+git push -u origin $INPUT_REPO_BRANCH
