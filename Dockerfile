41100041c8b82e688f3aaa9a8d0341edc8e05524
minoplhy <zebr@m21.kylz.nl>
2021-08-01 10:17:18 +0700

    Fix Attempt #4?

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM debian
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY filters.sh /filters.sh
-RUN apt-get update && apt-get install -y curl && apt-get install -y git
+RUN apt-get update && apt-get install -y python3 && apt-get install -y git
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 RUN chmod 777 /filters.sh
 ENTRYPOINT ["/bin/bash","/filters.sh"]
--- filters.sh
+++ filters.sh
@@ -2,9 +2,9 @@
 git clone https://github.com/minoplhy/filters-maker /filters-maker
 git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_REPO.git /$Destination_NAME
 cd /
-filters-maker/crawler.py filters-maker/input/domains.txt
-filters-maker/maker-rpz.py filtes-maker/input/domains.txt $Destination_REPO/$Destination_VERSION/rpz.txt
-cd $Destination_REPO
+python3 filters-maker/crawler.py filters-maker/input/domains.txt
+python3 filters-maker/maker-rpz.py filtes-maker/input/domains.txt $Destination_REPO/$Destination_VERSION/rpz.txt
+cd $Destination_NAME
 git remote add destination https://x-access-token:$API_TOKEN_GITHUB@github.com/$Destination_REPO.git
 git config --local user.name $GIT_NAME
 git config --local user.email $GIT_EMAIL
