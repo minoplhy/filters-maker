917b475e365b7da45013c7c13bd991cd6dbee50e
minoplhy <zebr@m21.kylz.nl>
2021-08-01 11:24:39 +0700

    Fixing Attempt #13?

--- filters.sh
+++ filters.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/minoplhy/filters-maker /filters-maker
 git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_UNAME/$INPUT_DESTINATION_REPO.git /repros
-mkdir /repros/Private-Build/$INPUT_DESTINATION_VERSION
+mkdir /repros/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION
 pip3 install -r /filters-maker/requirements.txt
 python3 /filters-maker/crawler.py /filters-maker/input/domains.txt
 python3 /filters-maker/maker-rpz.py /filters-maker/input/excluded.txt /filters-maker/input/domains.txt /repros/Private-Build/$INPUT_DESTINATION_VERSION/rpz.txt
