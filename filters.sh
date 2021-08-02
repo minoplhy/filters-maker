582026c22b1fb4ab082b1db7505e2cf4a5fecb28
Kale <zebr@m21.kylz.nl>
2021-08-02 07:09:13 -0700

    Update 'filters.sh'

--- filters.sh
+++ filters.sh
@@ -4,13 +4,7 @@ git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION
 mkdir /repros/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION
 pip3 install -r /filters-maker/requirements.txt
 cd /repros
-cp /filters-maker/crawler.py /repros/crawler.py
-cp /filters-maker/maker_rpz.py /repros/maker_rpz.py
-cp /filters-maker/maker_domains.py /repros/maker_domains.py
 python3 /filters-maker/$INPUT_ACTION_FILE
-rm /repros/crawler.py
-rm /repros/maker_rpz.py
-rm /repros/maker_domains.py
 git config --local user.name $INPUT_GIT_NAME
 git config --local user.email $INPUT_GIT_EMAIL
 git add .
