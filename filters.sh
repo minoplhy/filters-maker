a6ce98bdff7daeca755a13d8cc1f3f937e2504f9
minoplhy <zebr@m21.kylz.nl>
2021-08-02 18:55:33 +0700

    action

--- filters.sh
+++ filters.sh
@@ -4,7 +4,7 @@ git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION
 mkdir /repros/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION
 pip3 install -r /filters-maker/requirements.txt
 cd /repros
-python3 /repros/$INPUT_ACTION_FILE
+python3 /filters-maker/$INPUT_ACTION_FILE
 git config --local user.name $INPUT_GIT_NAME
 git config --local user.email $INPUT_GIT_EMAIL
 git add .
