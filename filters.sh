43118f05988fc21297942260b6f868da89c794fc
Kale <zebr@m21.kylz.nl>
2021-08-02 05:50:20 -0700

    Update 'filters.sh'

--- filters.sh
+++ filters.sh
@@ -5,12 +5,12 @@ mkdir /repros/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION
 pip3 install -r /filters-maker/requirements.txt
 cd /repros
 cp /filters-maker/crawler.py /repros/crawler.py
-cp /filters-maker/maker-rpz.py /repros/maker-rpz.py
-cp /filters-maker/maker-domains.py /repros/maker-domains.py
+cp /filters-maker/maker_rpz.py /repros/maker_rpz.py
+cp /filters-maker/maker_domains.py /repros/maker_domains.py
 python3 /filters-maker/$INPUT_ACTION_FILE
 rm /repros/crawler.py
-rm /repros/maker-rpz.py
-rm /repros/maker-domains.py
+rm /repros/maker_rpz.py
+rm /repros/maker_domains.py
 git config --local user.name $INPUT_GIT_NAME
 git config --local user.email $INPUT_GIT_EMAIL
 git add .
