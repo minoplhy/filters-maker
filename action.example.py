6eb09a9db9ef10e77b3ba0b9efbddeeb30d64f75
Kale <zebr@m21.kylz.nl>
2021-08-02 09:13:42 -0700

    Update with current script

--- action.example.py
+++ action.example.py
@@ -5,21 +5,22 @@ import crawler
 import maker_rpz
 import maker_domains
 
-input = '/repros/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION/domains.txt'
-excluded = '/repros/Resources/excluded.txt'
-rpz_locat = '/repros/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION/rpz.txt'
+incoming = "/repros/Private-build/ucate/domains.txt"
+excluded = "/repros/Resources/excluded.txt"
+rpz_locat = "/repros/Private-build/ucate/rpz.txt"
 
-crawler.download_filters("https://dbl.oisd.nl/" ,input)
-crawler.download_filters("https://hosts.netlify.app/Pro/rpz.txt" ,input)
-crawler.download_filters("https://filters.kylz.nl/RPZ/adguard/dns.txt" ,input)
-crawler.download_filters("https://filters.kylz.nl/RPZ/adguard/cname-tracker.txt" ,input)
-crawler.download_filters("https://filters.kylz.nl/RPZ/adguard/cname-original.txt" ,input)
-crawler.download_filters("https://filters.kylz.nl/RPZ/stevenblack/f-s.txt" ,input)
-crawler.download_filters("https://filters.kylz.nl/RPZ/someonewhocares/rpz.txt" ,input)
-crawler.download_filters("https://urlhaus.abuse.ch/downloads/rpz/" ,input)
-crawler.download_filters("https://github.com/easylist/easylist/raw/master/easylist/easylist_adservers.txt" ,input)
-crawler.filtering(input)
-crawler.filteringcon(input)
-crawler.killingdup(input)
-maker_rpz.RPZbuilding(excluded, input, rpz_locat)
-maker_domains.domainsbuilding(excluded, input)
+crawler.download_filters("https://dbl.oisd.nl/" ,incoming)
+crawler.download_filters("https://hosts.netlify.app/Pro/rpz.txt" ,incoming)
+crawler.download_filters("https://filters.kylz.nl/RPZ/adguard/dns.txt" ,incoming)
+crawler.download_filters("https://filters.kylz.nl/RPZ/adguard/cname-tracker.txt" ,incoming)
+crawler.download_filters("https://filters.kylz.nl/RPZ/adguard/cname-original.txt" ,incoming)
+crawler.download_filters("https://filters.kylz.nl/RPZ/stevenblack/f-s.txt" ,incoming)
+crawler.download_filters("https://filters.kylz.nl/RPZ/someonewhocares/rpz.txt" ,incoming)
+crawler.download_filters("https://urlhaus.abuse.ch/downloads/rpz/" ,incoming)
+crawler.download_filters("https://github.com/easylist/easylist/raw/master/easylist/easylist_adservers.txt" ,incoming)
+crawler.filtering(incoming)
+crawler.filteringcon(incoming)
+crawler.killingdup(incoming)
+crawler.excluded(excluded, incoming)
+maker_rpz.RPZbuilding(excluded, incoming, rpz_locat)
+maker_domains.domainsbuilding(excluded, incoming)
