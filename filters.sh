429aa6c46497c7ff35b511cc10956b67f74e03cc
Kale <zebr@m21.kylz.nl>
2021-08-06 19:35:04 -0700

    test reprwiki

--- filters.sh
+++ filters.sh
@@ -4,7 +4,7 @@ git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION
 git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_UNAME/$INPUT_DESTINATION_REPO.wiki.git /reprwiki
 mkdir /repros/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION
 pip3 install -r /filters-maker/requirements.txt
-cd /repros
+cd /reprwiki
 python3 /repros/$INPUT_ACTION_FILE
 git config --local user.name $INPUT_GIT_NAME
 git config --local user.email $INPUT_GIT_EMAIL
