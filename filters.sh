2174d0fa439bfaae8679d5233833f61061c2ce4f
Kale <zebr@m21.kylz.nl>
2021-08-06 19:40:54 -0700

    Update 'filters.sh'

--- filters.sh
+++ filters.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/minoplhy/filters-maker /filters-maker
 git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_UNAME/$INPUT_DESTINATION_REPO.git /repros
 git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_UNAME/$INPUT_DESTINATION_REPO.wiki.git /reprwiki
+mkdir /reprwiki/$INPUT_DESTINATION_FOLDER/
 mkdir /reprwiki/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION
 pip3 install -r /filters-maker/requirements.txt
 cd /reprwiki
