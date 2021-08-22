3060f058964769008d329a7c9149dcf91fc9fa87
Kale <zebr@m21.kylz.nl>
2021-08-21 19:36:45 -0700

    Update 'filters.sh'

--- filters.sh
+++ filters.sh
@@ -7,16 +7,17 @@ mkdir /reprwiki/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION
 pip3 install -r /filters-maker/requirements.txt
 cd /reprwiki
 python3 /repros/$INPUT_ACTION_FILE
+TIMEDATE=`date`
 git config --local user.name $INPUT_GIT_NAME
 git config --local user.email $INPUT_GIT_EMAIL
 git add .
-git commit -m "Schedule Building : `date`"
+git commit -m "Schedule Building : $TIMEDATE"
 git push -u origin $INPUT_REPO_BRANCH
 cd /repros
 git config --local user.name $INPUT_GIT_NAME
 git config --local user.email $INPUT_GIT_EMAIL
 git add .
-git commit -m "Schedule Building : `date`"
+git commit -m "Schedule Building : $TIMEDATE"
 git push -u origin $INPUT_BRANCH_VERSION
 if [ -f "/repros/$INPUT_SUB_ACTION_LOCATION" ]; then
     echo $API_TOKEN_GITHUB > token.txt
@@ -25,5 +26,6 @@ if [ -f "/repros/$INPUT_SUB_ACTION_LOCATION" ]; then
     python3 /repros/$INPUT_SUB_ACTION_LOCATION
     echo "Code Completed!"
     gh release delete latest -y
-    gh release create latest -F /repros/Resources/Releases.md /gh-releases/*
+    git tag | xargs -L 1 | xargs git push origin --delete
+    gh release create latest -F /repros/Resources/Releases.md /gh-releases/* -t FILTERS IN RELEASES
 fi
