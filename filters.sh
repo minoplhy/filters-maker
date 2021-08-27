649ccefc3081d915b99b407c4e1150ad5138ff62
minoplhy <zebr@m21.kylz.nl>
2021-08-27 12:34:04 +0700

    wiki shutoff

--- filters.sh
+++ filters.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 git clone https://github.com/minoplhy/filters-maker /filters-maker
 git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_UNAME/$INPUT_DESTINATION_REPO.git /repros
-git clone https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_UNAME/$INPUT_DESTINATION_REPO.wiki.git /reprwiki
+mkdir /reprwiki
 mkdir /reprwiki/$INPUT_DESTINATION_FOLDER/
 mkdir /reprwiki/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION
 pip3 install -r /filters-maker/requirements.txt
 cd /reprwiki
 python3 /repros/$INPUT_ACTION_FILE
-TIMEDATE=`date`
-git config --local user.name $INPUT_GIT_NAME
-git config --local user.email $INPUT_GIT_EMAIL
-git add .
-git commit -m "Schedule Building : $TIMEDATE"
-git push -u origin $INPUT_REPO_BRANCH
 cd /repros
 git config --local user.name $INPUT_GIT_NAME
 git config --local user.email $INPUT_GIT_EMAIL
