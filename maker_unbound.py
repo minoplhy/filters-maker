cf9a976075f4f09066d1dbcb60fa048fe4c49831
minoplhy <hej@aspoblourib.1w1.one>
2021-09-29 22:10:01 +0700

    Maker Fix

--- maker_unbound.py
+++ maker_unbound.py
@@ -28,30 +28,9 @@ def UNBbuilding(excluded ,incoming,Version):
         f.write('# Compiled Date : ' + str(date) +'\n\n')
         for line in lines:
             if line.strip() and not line in exclude and not line.startswith('#'):
-               f.write('\n'.join([line + '\n']))
+               f.write('\n'.join(['local-zone: "' + line + '" always_refuse\n']))
             elif line.startswith((';','$','@','  IN')):
                f.write('\n'.join([line + '\n']))
             elif not line.strip():
-               f.write('\n'.join([line + '\n']))
-    f.close()
-
-def DMallowlist(incoming, output,Version):
-    ankstanop = linecounter(incoming)
-    with open(incoming ,'r') as f:
-        lines = f.read().splitlines() # read lines
-    with open(output ,'w') as f:
-        f.write('# Title : Minoplhy Personal Blocklist\n')
-        f.write('# Description : My Very Personal DNS Blocklist plus crawling from the source\n')
-        f.write('# Source : Resources/Source.txt\n')
-        f.write('# Rule Counter : ' + str(ankstanop) +' Rules\n')
-        f.write('# Format : Unbound\n')
-        f.write('# Type : Allowlist\n')
-        f.write('# Version : ' + str(Version) +'\n')
-        f.write('# Licenses : MIT\n')
-        f.write('# Compiled Date : ' + str(date) +'\n\n')
-        for line in lines:
-            if line.strip() and not line.startswith(';'):
-               f.write('\n'.join(['local-zone: "' + line + '" always_refuse\n']))
-            elif line.startswith((';','$','@','  IN')):
                f.write('\n'.join([line + '\n']))
     f.close()
