ab076e13bcd29971cb8a34e0855091ce13492ee8
minoplhy <hej@aspoblourib.1w1.one>
2021-11-05 20:56:12 +0700

    build_make custom extension and default is txt

--- build_make.py
+++ build_make.py
@@ -2,12 +2,12 @@ import os
 import glob
 import shutil
 
-def filepath_mass_changer(Version ,path ,apapath):
+def filepath_mass_changer(Version ,path ,apapath, extension="txt"):
     f = glob.glob(os.path.join(path,"*.txt"))
     for filename in f:
         s = os.path.basename(filename)
         os.rename(filename, os.path.join(path, Version + '_' + s ))
-        f = glob.glob(os.path.join(path,"*.txt"))
+        f = glob.glob(os.path.join(path,"*."+extension))
     for filename in f:
         s = os.path.basename(filename)
         shutil.copy2(os.path.join(path,s), apapath)
