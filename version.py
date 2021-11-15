f3a823e5fc7bed561b7a869802e94256da4626f7
minoplhy <hej@aspoblourib.1w1.one>
2021-11-15 10:37:52 +0700

    version.py : convert to str

--- version.py
+++ version.py
@@ -7,5 +7,5 @@ date = datetime.datetime.now(UTC)
 
 def build(incoming, addition=""):
     with open(incoming, 'w') as f:
-        f.write('\n'.join([str(date)]+addition))
+        f.write('\n'.join([str(date)]+str(addition)))
     f.close()
