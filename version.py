c6c27ce680d69a0eca8f14fd360ffdecae642a06
minoplhy <hej@aspoblourib.1w1.one>
2021-11-15 08:51:05 +0700

    version : add addition

--- version.py
+++ version.py
@@ -5,7 +5,7 @@ import pytz
 UTC = pytz.utc
 date = datetime.datetime.now(UTC)
 
-def build(incoming):
+def build(incoming, addition=""):
     with open(incoming, 'w') as f:
-        f.write('\n'.join([str(date)]))
+        f.write('\n'.join([str(date)]+addition))
     f.close()
